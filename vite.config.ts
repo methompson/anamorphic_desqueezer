99a9644a57c1275616ff60c6a6f2e28a83c38cc3
Mat Home <home@Mathews-MacBook-Air.local>
2026-01-14 22:00:34 -0600

    Added PWA stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,40 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+      manifest: {
+        name: 'Anamorphic Desqueezer',
+        short_name: 'Anamorphic Desqueezer',
+        description:
+          'Anamorphic Desqueezer is a tool for desqueezing anamorphic images',
+        theme_color: '#B10047',
+        icons: [
+          {
+            src: 'anamorph_logo_1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+          },
+          {
+            src: 'anamorph_logo_180.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ['@metools/web-image-converter'],
   },
